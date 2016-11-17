9e0e02eaed3b251d197e6fafc8041761ccb9053e
Tomoya Miyara <e165709@ie.u-ryukyu.ac.jp>
2016-11-17 17:55:03 +0900

    version 1.0

---
+++ src/test/jp/ac/uryukyu/ie/e165709/EnemyTest.java
@@ -0,0 +1,20 @@
+package jp.ac.uryukyu.e165709;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EnemyTest {
+    @Test
+    public void attack() throws Exception {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.dead = true;
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.hitPoint);
+    }
+
+}
