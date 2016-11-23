32a3cf939f6b281f27dc57682358e877897256ea
Tomoya Miyara <e165709@ie.u-ryukyu.ac.jp>
2016-11-23 12:53:23 +0900

    version 1.0

--- src/main/jp/ac/uryukyu/ie/e165709/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165709/Main.java
@@ -2,7 +2,7 @@ package jp.ac.uryukyu.ie.e165709;
 
 public class Main {
     public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
+        Hero hero = new Hero("勇者", 6, 5);
         Enemy enemy = new Enemy("スライム", 6, 3);
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
