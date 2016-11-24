753965341eb42949a45a26ac97facc9281428207
Tomoya Miyara <e165709@ie.u-ryukyu.ac.jp>
2016-11-24 17:54:06 +0900

    version 1.2

--- src/main/jp/ac/uryukyu/ie/e165709/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e165709/Enemy.java
@@ -1,41 +1,8 @@
 package jp.ac.uryukyu.ie.e165709;
 
-public class Enemy {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+public class Enemy extends LivingThing{
 
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        super(name, maximumHP, attack);
     }
-
-    public boolean isDead() {
-        return dead;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void attack(Hero hero){
-        if (dead == false){
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
-    }
-
-    public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("モンスター%sは倒れた。\n", name);
-        }
-    }
-
 }
